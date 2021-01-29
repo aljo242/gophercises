d09d4df6b9d799849d3e764fa30900bc138b8854
aljo242 <amj92@pitt.edu>
2021-01-29 13:30:42 -0500

    finish exercise 1

--- exercise1/main.go
+++ exercise1/main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"encoding/csv"
-	"os"
 	"flag"
-	"strings"
+	"fmt"
+	"math/rand"
+	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 const (
-	defaultName = "./test_input.csv"
-	fileSuffix = ".csv"
+	defaultName          = "./test_input.csv"
+	fileSuffix           = ".csv"
+	defaultTimer float64 = 30.0
 )
 
 var (
-	userInput = flag.Bool("f", false, "Enable user input file")
-	filename string = defaultName
+	userInput           = flag.Bool("f", false, "Enable user input file")
+	timer               = flag.Float64("t", defaultTimer, "Timer for the quiz (seconds)")
+	shuffleFlag         = flag.Bool("s", false, "Shuffle the order of the parsed quiz")
+	filename    string  = defaultName
+	timerLength float64 = defaultTimer
 )
 
-func readUserFile()  {
+func readUserFile() {
 	fmt.Println("Enter the file you want to use: ")
 	fmt.Scanln(&filename)
 
@@ -30,11 +36,29 @@ func readUserFile()  {
 	}
 }
 
+func shuffleQuestions(questions [][]string) [][]string {
+	fmt.Println("Shuffling questions...")
+	rand.Shuffle(len(questions), func(i, j int) {
+		questions[i], questions[j] = questions[j], questions[i]
+	})
+
+	return questions
+}
 
 func main() {
-	flag.Parse()	
+	rand.Seed(time.Now().Unix())
+
+	flag.Parse()
 	if *userInput == true {
 		readUserFile()
+	}
+
+	if *timer < 0 {
+		fmt.Println("Timer flag was a non-positive value.")
+		fmt.Println("Setting timer value to", defaultTimer, "seconds")
+
+	} else {
+		timerLength = defaultTimer
 	}
 
 	fmt.Println("Opening", filename, "...")
@@ -51,20 +75,48 @@ func main() {
 
 	numQuestions := len(records)
 	numCorrect := 0
-	for _, line := range(records) {
-		question := line[0]
-		answer, err := strconv.Atoi(line[1])
-		if err != nil {
-			fmt.Println("Error converting token to intger type")
-		}
-		var userAnswer int
-		fmt.Printf("%v = ", question)
-		fmt.Scanf("%d", &userAnswer)
-		if userAnswer == answer {
-			numCorrect++
-			fmt.Println("CORRECT")
-		}
+
+	if *shuffleFlag {
+		records = shuffleQuestions(records)
 	}
 
-	fmt.Println(numCorrect, "out of", numQuestions)
+	processQuestions := func(ch chan<- struct{}) {
+		for _, line := range records {
+			question := line[0]
+			answer, err := strconv.Atoi(line[1])
+			if err != nil {
+				fmt.Println("Error converting token to intger type")
+			}
+			var userAnswer int
+			fmt.Printf("%v = ", question)
+			fmt.Scanf("%d", &userAnswer)
+			if userAnswer == answer {
+				numCorrect++
+			}
+		}
+		ch <- struct{}{}
+	}
+
+	signalChannel := make(chan struct{})
+	defer close(signalChannel) // defer chan close
+
+	delayTime := 30
+	go processQuestions(signalChannel)
+
+	timer := time.NewTimer(time.Duration(delayTime) * time.Second)
+	defer timer.Stop() // handle this resource
+
+	select {
+	case <-signalChannel:
+	case <-timer.C:
+		fmt.Printf("\nTimeout after %ds\n", delayTime)
+	}
+
+	fmt.Println(numCorrect, "correct out of", numQuestions)
+
+	// go run timer
+	// select a timer message
+	// otherwise process input
+	// case timerMessage -> break out and print message
+
 }
